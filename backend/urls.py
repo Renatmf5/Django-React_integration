daee324241642f2d9750a9131bdf65fa62c6bd6e
Renato Moreno Fereira <renato_mf5@hotmail.com>
2024-01-03 17:04:13 -0300

    first commit preparing the project

---
+++ backend/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework import routers
+from .views import UserViewSet, GroupViewSet
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'groups', GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+]
